2b7db080c8a52eb5492d3ced75cce2c415bbac19
github username <github email address>
2013-10-03 19:59:03 -0400

    kill and remake db to avoid using 'date' type

--- 10-3_kickstarter/schema.sql
+++ 10-3_kickstarter/schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   category TEXT,
   funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users(
