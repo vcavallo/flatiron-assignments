83bab426d43472c0aee109dcf67cbdbcf7770e37
github username <github email address>
2013-10-03 19:49:18 -0400

    create schema

---
+++ 10-3_kickstarter/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
