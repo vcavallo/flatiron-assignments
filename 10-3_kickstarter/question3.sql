e4ae1073809dab79407ec8b9b0e66d565bda5fb6
github username <github email address>
2013-10-04 09:06:06 -0400

    fix question3. all done\!

--- 10-3_kickstarter/question3.sql
+++ 10-3_kickstarter/question3.sql
@@ -1,5 +1,10 @@
 -- 3. Select the titles of all projects that have met their funding goal.
 
--- Without doing the math on paper and hard-coding the projects that have
--- met their goal, I can't figure out how to do this with SQL:
--- select all titles where pledges for that id >= funding_goal...
+SELECT title, SUM(pledges.amount) AS `total_amount_pledged`
+  FROM projects 
+  JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE pledges.amount >= projects.funding_goal
+  GROUP BY projects.id
+;
+
