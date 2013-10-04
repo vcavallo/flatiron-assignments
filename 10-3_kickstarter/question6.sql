c9588408b856899d066caba1fb44a4e0d2542ae6
github username <github email address>
2013-10-04 00:54:16 -0400

    finish question6

--- 10-3_kickstarter/question6.sql
+++ 10-3_kickstarter/question6.sql
@@ -1,2 +1,11 @@
--- 6. Select the category names and the sum total of the pledge amounts of all the pledges in the book category.
+-- 6. Select the category names and the sum total of the pledge amounts 
+-- of all the pledges in the book category.
 
+SELECT title, category, SUM(pledges.amount)
+  FROM projects 
+  JOIN pledges
+  ON projects.id = pledges.user_id
+  WHERE category = "books"
+;
+
+-- not sure why it's only showing one book title. 
