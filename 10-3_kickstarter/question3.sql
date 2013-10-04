689db46760fe1f03386f6d2155af325c14bc0576
github username <github email address>
2013-10-03 21:27:46 -0400

    complete question4

--- 10-3_kickstarter/question3.sql
+++ 10-3_kickstarter/question3.sql
@@ -1,1 +1,7 @@
 -- 3. Select the titles of all projects that have met their funding goal.
+
+SELECT projects.title
+  FROM pledges 
+  JOIN projects 
+  ON pledges.project_id = projects.id 
+;
--- 10-3_kickstarter/question4.sql
+++ 10-3_kickstarter/question4.sql
@@ -1,1 +1,8 @@
 -- 4. Select user names and amounts of all pledges. Order them by the amount.
+
+SELECT amount, users.name
+  FROM pledges 
+  JOIN users 
+  ON pledges.user_id = users.id
+  ORDER BY amount DESC
+;
