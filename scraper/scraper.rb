10f967e057b8f876c0caf04bbfb44077f09a63dd
github username <github email address>
2013-10-04 15:55:28 -0400

    begin working on individual pages

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -6,14 +6,6 @@ doc = Nokogiri::HTML(open("http://students.flatironschool.com/"))
 
 all_student_data = doc.css("li.home-blog-post")
 
-# stuff for a single student
-
-# student.css("div.big-comment").first.text
-# student_page_link = student.css("a").first.attr("href")
-# student_image_link = student.css("img").first.attr("src")
-# student_tagline = student.css("p.home-blog-post-meta").first.text
-# student_excerpt = student.css("div.excerpt").css("p").first.text
-
 all_student_links = all_student_data.collect do |student|
   student.css("a").attr("href")
 end
@@ -21,5 +13,23 @@ end
 all_student_image_links = all_student_data.collect do |student|
   student.css("img").attr("src")
 end
+
+all_student_taglines = all_student_data.collect do |student|
+  student.css("p.home-blog-post-meta").text
+end
+
+all_student_excerpt = all_student_data.collect do |student|
+  student.css("div.excerpt").css("p").text
+end
+
+# # loop through all student pages
+# all_student_links.each do |url|
+
+#   student_page = Nokogiri::HTML(open("http://students.flatironschool.com/#{url}"))
+
+# end
+
+
+
 
 binding.pry
