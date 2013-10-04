f911d6925ae3e2a17fe18ca58a4e7b1a6b599ee6
github username <github email address>
2013-10-03 22:26:00 -0400

    complete question5

--- 10-3_kickstarter/question5.sql
+++ 10-3_kickstarter/question5.sql
@@ -1,1 +1,8 @@
 -- 5. Select the category names, and pledge amounts of all pledges in the music category.
+
+SELECT category, pledges.amount
+  FROM projects 
+  JOIN pledges
+  ON projects.id = pledges.user_id
+  WHERE category = "music"
+;
