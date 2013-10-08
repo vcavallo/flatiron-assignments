ce6740f0c381c151ac7c06b4f21f0bd5e2aaeefe
github username <github email address>
2013-10-08 10:20:40 -0400

    begin oojukebox

---
+++ oojukebox.rb
@@ -0,0 +1,16 @@
+class Jukebox
+
+  # 
+
+
+end
+
+songs = [
+  "Melvins - Gluey Porch Treatments",
+  "Charles Mingus - Goodbye, Porkpie Hat",
+  "Slayer - Angel of Death",
+  "Raffi - Baby Beluga"
+  ]
+
+j = Jukebox.new(songs)
+j.call
