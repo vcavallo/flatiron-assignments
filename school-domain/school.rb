51789858683126937b20bbae2d3052098def4bde
github username <github email address>
2013-10-08 22:18:38 -0400

    finishing school domain

--- school-domain/school.rb
+++ school-domain/school.rb
@@ -1,19 +1,27 @@
 require 'pry'
 
 class School
-
-  @roster = {1 => ["tom","dick"]}
+  attr_accessor :roster, :school_name
 
   def initialize(school_name)
     @school_name = school_name
+    @roster = {}
   end
 
   def add_student(name, grade)
-    @roster[grade]  
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
   end
 
   def grade(grade)
     @roster[grade]
+  end
+
+  def sort
+    @roster.keys.sort
   end
 
 end
